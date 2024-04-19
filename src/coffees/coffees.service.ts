89a3cb029b410f527b88352359c865e27aea7924
Jake OuYang <jakeouyang@hotmail.com>
2024-04-19 23:54:31 +0800

    how to use jest | import 不要使用绝对地址，测试会出引用问题

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,7 +1,6 @@
 import {
   HttpException,
   HttpStatus,
-  Inject,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
@@ -11,10 +10,8 @@ import { DataSource, Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Flavor } from './entities/flavor.entity';
-import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
-import { Event } from 'src/events/entities/event.entity';
-import coffeesConfig from './config/coffees.config';
-import { ConfigType } from '@nestjs/config';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
+import { Event } from '../events/entities/event.entity';
 
 @Injectable()
 export class CoffeesService {
@@ -24,8 +21,6 @@ export class CoffeesService {
     @InjectRepository(Flavor)
     private readonly flavorRepository: Repository<Flavor>,
     private readonly dataSource: DataSource,
-    @Inject(coffeesConfig.KEY)
-    private readonly coffeesConfiguration: ConfigType<typeof coffeesConfig>,
   ) {
     // console.log(this.coffeesConfiguration);
   }
